0207f4aa59beca219ddc4a69fd4849fee21ec3b4
unknown <shalakhasardana18@gmail.com>
2020-09-17 23:39:05 -0700

    Activity Indicator

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -4,6 +4,7 @@ import { Text } from "react-native";
 import { Card } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
@@ -13,7 +14,18 @@ const mapStateToProps = (state) => {
   };
 };
 
-function RenderItem({ item }) {
+function RenderItem(props) {
+  const { item } = props;
+  if (props.isLoading) {
+    return <Loading />;
+  }
+  if (props.errMess) {
+    return (
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>
+    );
+  }
   if (item) {
     return (
       <Card featuredTitle={item.name} image={{ uri: baseUrl + item.image }}>
@@ -38,6 +50,8 @@ class Home extends Component {
               (campsite) => campsite.featured
             )[0]
           }
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
         />
         <RenderItem
           item={
@@ -45,6 +59,8 @@ class Home extends Component {
               (promotion) => promotion.featured
             )[0]
           }
+          isLoading={this.props.promotions.isLoading}
+          errMess={this.props.promotions.errMess}
         />
         <RenderItem
           item={
@@ -52,6 +68,8 @@ class Home extends Component {
               (partner) => partner.featured
             )[0]
           }
+          isLoading={this.props.partners.isLoading}
+          errMess={this.props.partners.errMess}
         />
       </ScrollView>
     );
