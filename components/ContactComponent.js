e67e2b84b7c12111c35fcc406a2816e5830b4fc2
unknown <shalakhasardana18@gmail.com>
2020-10-01 15:36:00 -0700

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
-import { ScrollView, View, Text } from "react-native";
-import { Card } from "react-native-elements";
-import * as Animatable from "react-native-animatable";
+import React, { Component } from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
   constructor(props) {
@@ -10,18 +11,39 @@ class Contact extends Component {
   }
 
   static navigationOptions = {
-    title: "Contact Us",
+    title: 'Contact Us',
   };
+
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ['campsites@nucamp.co'],
+      subject: 'Inquiry',
+      body: 'To whom it may concern:',
+    });
+  }
 
   render() {
     return (
       <ScrollView>
-        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
-          <Card wrapperStyle={{ margin: 20 }} title="Contact Information">
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+          <Card wrapperStyle={{ margin: 20 }} title='Contact Information'>
             <Text> 1 Nucamp Way</Text>
             <Text> Seattle, WA 98001</Text>
             <Text style={{ marginBottom: 10 }}> Phone: 1-206-555-1234 </Text>
             <Text> Email: campsites@nucamp.co</Text>
+            <Button
+              title='Send Email'
+              buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+              icon={
+                <Icon
+                  name='envelope-o'
+                  type='font-awesome'
+                  color='#fff'
+                  iconStyle={{ marginRight: 10 }}
+                />
+              }
+              onPress={() => this.sendMail()}
+            />
           </Card>
         </Animatable.View>
       </ScrollView>
